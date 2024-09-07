42960073871d579c6ded15c49cdeb4a377954beb
enryu-maker <akifkhan60067@gmail.com>
2024-09-07 16:06:18 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react';
 function App() {
+  const [privateIP, setPrivateIP] = useState(null);
+
+  React.useEffect(() => {
+    const getPrivateIP = () => {
+      const RTCPeerConnection =
+        window.RTCPeerConnection ||
+        window.mozRTCPeerConnection ||
+        window.webkitRTCPeerConnection;
+
+      if (!RTCPeerConnection) {
+        console.error("WebRTC not supported by this browser.");
+        return;
+      }
+
+      const pc = new RTCPeerConnection({ iceServers: [] });
+      const noop = () => {};
+
+      pc.createDataChannel(""); // create a bogus data channel
+      pc.createOffer()
+        .then(offer => pc.setLocalDescription(offer))
+        .catch(noop); // create offer and set local description
+
+      pc.onicecandidate = (ice) => {
+        if (ice && ice.candidate && ice.candidate.candidate) {
+          const candidate = ice.candidate.candidate;
+          const ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3})/;
+          const ipMatch = candidate.match(ipRegex);
+
+          if (ipMatch) {
+            const ip = ipMatch[1];
+            setPrivateIP(ip);
+          } else if (candidate.includes('.local')) {
+            setPrivateIP("mDNS protection in place: Private IP hidden");
+          }
+
+          pc.onicecandidate = noop;
+        }
+      };
+    };
+
+    getPrivateIP();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Your Private IP Address: {privateIP ? privateIP : "Fetching..."}</h3>
     </div>
   );
-}
-
+};
 export default App;
